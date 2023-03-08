4653eb3f260fc1e5e00fe23063f141d484bd79d6
Avalon1997 <461879033@qq.com>
2023-03-08 17:08:52 +0800

    更改 库文件 —— 20230308

--- WaterQulityDataExport/ExportWaterData.c
+++ WaterQulityDataExport/ExportWaterData.c
@@ -16,27 +16,40 @@
 
 /* ---------- Include ---------- */
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <string.h>
+// #include <wiringPi.h>
+// #include <wiringSerial.h>
 #include <unistd.h>
+// #include <string.h>
 // #include <windows.h>
 
 
-
-
-
-
-
-void main(void)
+ 
+int main(void)
 {
-    printf("Hello World !!!\n\n");
-
-    system("pause");
-
-
-} 
-
+	char buf[64] = {'\0'};
+	int n_read;
+	wiringPiSetup();    //调用此函数来进行初始化工作。
+	
+	int fd =serialOpen("/dev/ttyAMA1",115200); //打开串口
+	
+	if(fork() == 0){
+		while(1){
+			memset(buf,0,sizeof(buf));
+			n_read = read(fd,buf,sizeof(buf));
+			if(n_read != 0){
+				printf("data %dB:%s\n",n_read,buf); //子进程一直等待数据接收
+			}	
+		}
+	}
+    else{
+			while(1){
+				serialPrintf(fd,"hello world!!\n"); //每隔三秒发送一次hello world!!
+				sleep(3);
+			}
+	}
+ 
+ return 0;
+}
 
 
 
